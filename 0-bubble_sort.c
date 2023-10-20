e54a635aee7a892ccd4aff53909928de1265fd19
Elimali hamza <elimal.hamza@gmail.com>
2023-10-20 04:18:54 -0700

    betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,26 +1,41 @@
 #include "sort.h"
 
+/**
+ * swap - Swaps two integers
+ * @a: Pointer to the first integer
+ * @b: Pointer to the second integer
+ */
+
 void swap(int *a, int *b)
 {
-    size_t temp;
-    
-    temp = *a;
-    *a = *b;
-    *b = temp;
-    
+	size_t temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+
 }
+
+/**
+ * bubble_sort - Sorts an array of integers
+ * in ascending order using Bubble Sort
+ * @array: Pointer to the array to be sorted
+ * @size: The number of elements in the array
+ */
+
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    for (i = 0; i < size - 1; i++)
-    {
-        for (j = 0; j < size - 1 - i; j++)
-        { 
-            if (array[j] > array[j + 1])
-            {
-                swap(&array[j], &array[j + 1]);
-                print_array(array, size);
-            }
-        }
-    }
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+		}
+	}
 }
--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,36 +1,42 @@
 #include "sort.h"
 
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers using
+ * the Insertion sort algorithm
+ * @list: Pointer to a pointer to the head of the list
+ */
+
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *curr, *key, *prev;
-    
-    if (list == NULL || *list == NULL || (*list)->next == NULL) 
-        return;
-    
-    curr = (*list)->next;
-    
-    for(; curr; curr = curr->next)
-    {
-        key = curr;
-        prev = curr->prev;
-        
-        while (prev && prev->n > curr->n)
-        {
-            prev->next = key->next;
-            if (key->next != NULL)
-                key->next->prev = prev;
-                
-            key->next = prev;
-            key->prev = prev->prev;
-            prev->prev = key;
-            
-            if (key->prev != NULL)
-                key->prev->next = key;
-            else
-                *list = key;
+	listint_t *curr, *key, *prev;
 
-            prev = key->prev;
-            print_list(*list);
-        }
-    }
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	curr = (*list)->next;
+
+	for (; curr; curr = curr->next)
+	{
+		key = curr;
+		prev = curr->prev;
+
+		while (prev && prev->n > curr->n)
+		{
+			prev->next = key->next;
+			if (key->next != NULL)
+				key->next->prev = prev;
+
+			key->next = prev;
+			key->prev = prev->prev;
+			prev->prev = key;
+
+			if (key->prev != NULL)
+				key->prev->next = key;
+			else
+				*list = key;
+
+			prev = key->prev;
+			print_list(*list);
+		}
+	}
 }
