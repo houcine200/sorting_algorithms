a062659f3d0df5f0a26cbef740b6ca0f9ec27f76
Houcine Walaq <houcine.walaq@gmai.com>
2023-10-20 12:31:09 +0100

    2 mine

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,39 +1,32 @@
 #include "sort.h"
-
-void swap(int *a, int *b)
+void swapp(int *a, int *b)
 {
-	size_t temp;
-
-	temp = *a;
-	*a = *b;
-	*b = temp;
-
+    int temp;
+    
+    temp = *a;
+    *a = *b;
+    *b = temp;
+    
 }
+
 
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min;
-
-	for (i = 0; i < size; i++)
-	{
-
-		min = i;
-
-		for (j = i + 1; j < size; j++)
-		{
-
-			if (array[j] < array[min])
-			{
-
-				min = j;
-
-			}
-		}
-
-		if (min != i)
-			swap(&array[min], &array[i]);
-
-		print_array(array, size);
-
-	}
+    size_t i, j, min;
+    
+    for (i = 0; i < size - 1; i++)
+    {
+        min = i;
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[min])
+                min = j;
+        }
+            if (min != i)
+            {
+                swapp(&array[min], &array[i]);
+                print_array(array, size);
+            }
+    }
+    
 }
